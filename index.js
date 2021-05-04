3bd19f833768d220f302dc21eae1e5e7213eda4d
AwesomeCore1 <51826058+AwesomeCore1@users.noreply.github.com>
2021-05-04 17:49:26 +0200

    Ejs beginnigs

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    var mascots = [
+        { name: 'Sammy', organization: 'DigitalOcean', birth_year: 2012 },
+        { name: 'Tux', organization: 'Linux', birth_year: 1996 },
+        { name: 'Moby Duck', organization: 'Docker', birth_year: 2013 }
+    ];
+    var tagline = "No programming is complete without a cute animal mascot.";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
 app.get('/airports', function(req, res) {
