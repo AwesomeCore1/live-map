9f2ff5bd2401cd56c6d47c46ea681612f92febd6
Fredneyy <69763370+Fredneyy@users.noreply.github.com>
2021-12-19 13:55:35 +0100

    removed console logging

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.get('/clear', function(req, res) {
 
 io.on('connection', (socket) => {
 
-    console.log('a user connected');
+    // console.log('a user connected');
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        // console.log('user disconnected');
     });
 });
 
